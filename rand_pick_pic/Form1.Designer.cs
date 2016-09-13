697cf6321f61fb45ced1c5a3fae78c3cf4d8a237
blueblance <blueblance@gmail.com>
2016-09-13 16:25:37 +0800

    國考考題亂數取題軟體

--- rand_pick_pic/Form1.Designer.cs
+++ rand_pick_pic/Form1.Designer.cs
@@ -31,31 +31,43 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_A = new System.Windows.Forms.Button();
+            this.btn_B = new System.Windows.Forms.Button();
+            this.btn_D = new System.Windows.Forms.Button();
+            this.btn_c = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_simple_test = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 29);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(506, 613);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(904, 293);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(546, 448);
+            this.button1.Location = new System.Drawing.Point(987, 569);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "開始選取";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(546, 495);
+            this.button2.Location = new System.Drawing.Point(78, 154);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -64,17 +76,143 @@
             this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1094, 569);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "開始測驗";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_A
+            // 
+            this.btn_A.Location = new System.Drawing.Point(23, 53);
+            this.btn_A.Name = "btn_A";
+            this.btn_A.Size = new System.Drawing.Size(75, 23);
+            this.btn_A.TabIndex = 6;
+            this.btn_A.Text = "A";
+            this.btn_A.UseVisualStyleBackColor = true;
+            this.btn_A.Click += new System.EventHandler(this.btn_A_Click);
+            // 
+            // btn_B
+            // 
+            this.btn_B.Location = new System.Drawing.Point(24, 111);
+            this.btn_B.Name = "btn_B";
+            this.btn_B.Size = new System.Drawing.Size(75, 23);
+            this.btn_B.TabIndex = 7;
+            this.btn_B.Text = "B";
+            this.btn_B.UseVisualStyleBackColor = true;
+            this.btn_B.Click += new System.EventHandler(this.btn_B_Click);
+            // 
+            // btn_D
+            // 
+            this.btn_D.Location = new System.Drawing.Point(131, 111);
+            this.btn_D.Name = "btn_D";
+            this.btn_D.Size = new System.Drawing.Size(75, 23);
+            this.btn_D.TabIndex = 9;
+            this.btn_D.Text = "D";
+            this.btn_D.UseVisualStyleBackColor = true;
+            this.btn_D.Click += new System.EventHandler(this.btn_D_Click);
+            // 
+            // btn_c
+            // 
+            this.btn_c.Location = new System.Drawing.Point(131, 53);
+            this.btn_c.Name = "btn_c";
+            this.btn_c.Size = new System.Drawing.Size(75, 23);
+            this.btn_c.TabIndex = 8;
+            this.btn_c.Text = "C";
+            this.btn_c.UseVisualStyleBackColor = true;
+            this.btn_c.Click += new System.EventHandler(this.btn_c_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(77, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "測驗開始";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(987, 643);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "textbutton";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1102, 299);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "錯誤溫習";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btn_B);
+            this.panel1.Controls.Add(this.btn_A);
+            this.panel1.Controls.Add(this.btn_c);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btn_D);
+            this.panel1.Location = new System.Drawing.Point(956, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 263);
+            this.panel1.TabIndex = 13;
+            this.panel1.Visible = false;
+            // 
+            // btn_simple_test
+            // 
+            this.btn_simple_test.Location = new System.Drawing.Point(981, 299);
+            this.btn_simple_test.Name = "btn_simple_test";
+            this.btn_simple_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_simple_test.TabIndex = 11;
+            this.btn_simple_test.Text = "開始測驗";
+            this.btn_simple_test.UseVisualStyleBackColor = true;
+            this.btn_simple_test.Click += new System.EventHandler(this.btn_simple_test_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(63, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1292, 704);
-            this.Controls.Add(this.button2);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1255, 334);
+            this.Controls.Add(this.btn_simple_test);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -84,6 +222,17 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_A;
+        private System.Windows.Forms.Button btn_B;
+        private System.Windows.Forms.Button btn_D;
+        private System.Windows.Forms.Button btn_c;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_simple_test;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- rand_pick_pic/Form1.cs
+++ rand_pick_pic/Form1.cs
@@ -8,15 +8,20 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Collections;
+using System.Reflection;
 
 namespace rand_pick_pic
 {
     public partial class Form1 : Form
     {
         const int pick_pic_num = 4;
+        int[] pick_pic_num_A = {11 ,6 ,19, 4 ,5 , 5};
         private string pic_folder;
         private String[] FileCollection;
         int display_pic_num = 0;
+        string now_pic_path;
+        bool check_ans = false;
+        int correct_number = 0;
         ArrayList myArray = new ArrayList();
         public Form1()
         {
@@ -33,7 +38,7 @@ namespace rand_pick_pic
                 return;
             }
             pic_folder = openpic.SelectedPath;
-            FileCollection = Directory.GetFiles(pic_folder, "*.jpg");
+            FileCollection = Directory.GetFiles(pic_folder, "*.png");
             if (FileCollection.Length < pick_pic_num)
             {
                 MessageBox.Show("圖片太少了!!");
@@ -49,6 +54,7 @@ namespace rand_pick_pic
                 myArray.Add(FileCollection[number]);
             }
             Bitmap pic = new Bitmap(myArray[display_pic_num].ToString());
+            myArray[display_pic_num].ToString();
             pictureBox1.Image = pic;
             button2.Visible = true;
 
@@ -57,12 +63,300 @@ namespace rand_pick_pic
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (display_pic_num < pick_pic_num-1)
+            if(!check_ans)
+            {
+                MessageBox.Show("忘記填答案了");
+                return;
+            }
+            label1.Text = "";
+            next_pic_inarray();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            int number;
+            FolderBrowserDialog openpic = new FolderBrowserDialog();
+            if (openpic.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            panel1.Visible = true;
+            pic_folder = openpic.SelectedPath;
+            string[] dirs = Directory.GetDirectories(pic_folder);
+            for(int i = 0; i < dirs.Length; i++)
+            {
+                FileCollection = Directory.GetFiles(dirs[i], "*.png");
+                System.Random num = new Random();
+                for (int k = 0; k < pick_pic_num_A[i]; k++)
+                {
+                    do
+                    {
+                        number = num.Next(0, FileCollection.Length);
+                    } while (myArray.IndexOf(FileCollection[number]) != -1);
+                    myArray.Add(FileCollection[number]);
+                }
+            }
+            if (myArray.Count < 1)
+                return;
+            Bitmap pic = new Bitmap(myArray[display_pic_num].ToString());
+            now_pic_path = myArray[display_pic_num].ToString();
+            pictureBox1.Image = pic;
+            button2.Visible = true;            
+        }
+
+        private int consider_answer(string ans , string trueans)
+        {
+            string a = ans.Substring(ans.Length - 5, 1);            
+            if (a == trueans)
+                return 1;
+            else
+                return 0;
+        }
+
+        private void btn_A_Click(object sender, EventArgs e)
+        {
+            btn_default_color();            
+            if (consider_answer(now_pic_path, "A") == 1)
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = "正確";
+                next_pic_inarray();
+                if (!check_ans)
+                    correct_number++;
+            }
+            else
+            {
+                record_wrong_ans(now_pic_path);
+                label1.ForeColor = Color.Red;
+                label1.Text = "答錯了,正確答案是";
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "A")
+                {
+                    btn_A.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "B")
+                {
+                    btn_B.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "C")
+                {
+                    btn_c.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "D")
+                {
+                    btn_D.BackColor = Color.Green;
+                }
+            }
+            check_ans = true;
+        }
+
+        private void btn_B_Click(object sender, EventArgs e)
+        {
+            btn_default_color();
+            if (consider_answer(now_pic_path, "B") == 1)
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = "正確";
+                next_pic_inarray();
+                if (!check_ans)
+                    correct_number++;
+            }
+            else
+            {
+                record_wrong_ans(now_pic_path);
+                label1.ForeColor = Color.Red;
+                label1.Text = "答錯了,正確答案是";
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "A")
+                {
+                    btn_A.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "B")
+                {
+                    btn_B.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "C")
+                {
+                    btn_c.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "D")
+                {
+                    btn_D.BackColor = Color.Green;
+                }
+            }
+            check_ans = true;
+        }
+
+        private void btn_c_Click(object sender, EventArgs e)
+        {
+            btn_default_color();
+            if (consider_answer(now_pic_path, "C") == 1)
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = "正確";
+                next_pic_inarray();
+                if (!check_ans)
+                    correct_number++;
+            }
+            else
+            {
+                record_wrong_ans(now_pic_path);
+                label1.ForeColor = Color.Red;
+                label1.Text = "答錯了,正確答案是";
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "A")
+                {
+                    btn_A.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "B")
+                {
+                    btn_B.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "C")
+                {
+                    btn_c.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "D")
+                {
+                    btn_D.BackColor = Color.Green;
+                }
+            }
+            check_ans = true;
+        }
+
+        private void btn_D_Click(object sender, EventArgs e)
+        {
+            btn_default_color();
+            if (consider_answer(now_pic_path, "D") == 1)
+            {
+                label1.ForeColor = Color.Green;
+                label1.Text = "正確";
+                next_pic_inarray();
+                if (!check_ans)
+                    correct_number++;
+            }
+            else
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "答錯了,正確答案是";
+                record_wrong_ans(now_pic_path);
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "A")
+                {
+                    btn_A.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "B")
+                {
+                    btn_B.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "C")
+                {
+                    btn_c.BackColor = Color.Green;
+                }
+                if (now_pic_path.Substring(now_pic_path.Length - 5, 1) == "D")
+                {
+                    btn_D.BackColor = Color.Green;
+                }
+            }
+            check_ans = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            record_wrong_ans("hahaha");
+        }
+
+        private void btn_default_color()
+        {
+            btn_A.BackColor = Color.FromKnownColor(KnownColor.Control);
+            btn_B.BackColor = Color.FromKnownColor(KnownColor.Control);
+            btn_c.BackColor = Color.FromKnownColor(KnownColor.Control);
+            btn_D.BackColor = Color.FromKnownColor(KnownColor.Control);
+        }
+
+        private void record_wrong_ans(string wrong_path)
+        {
+            string str = System.IO.Directory.GetCurrentDirectory();
+            StreamWriter sw = File.AppendText(str + @"\wrong_ans\wrong_ans.txt");
+            sw.WriteLine(wrong_path);
+            sw.Flush();
+            sw.Close();              
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            button2.Visible = true;
+            string wrong_path;
+            string str = System.IO.Directory.GetCurrentDirectory();
+            StreamReader sw = new StreamReader(str+@"\wrong_ans\wrong_ans.txt");
+            myArray.Clear();
+            while ((wrong_path = sw.ReadLine()) != null)
+            {
+                myArray.Add(wrong_path);
+
+            }
+            if (myArray.Count < 1)
+                return;
+            Bitmap pic = new Bitmap(myArray[0].ToString());
+            now_pic_path = myArray[0].ToString();
+            pictureBox1.Image = pic;
+            sw.Close();
+            
+        }
+
+        private void next_pic_inarray()
+        {
+            check_ans = false;
+            btn_default_color();
+            int total_pick_num = 0;
+            for (int i = 0; i < pick_pic_num_A.Length; i++)
+            {
+                total_pick_num += pick_pic_num_A[i];
+            }
+            //if (display_pic_num < total_pick_num - 1)
+            if (display_pic_num < myArray.Count - 1)
                 display_pic_num++;
             else
+            {
                 display_pic_num = 0;
+                label2.Text = "你得到了" + ((correct_number * 2).ToString()) + " 分 ";
+                label2.Visible = true;
+            }
+                
+
+
             Bitmap pic = new Bitmap(myArray[display_pic_num].ToString());
+            now_pic_path = myArray[display_pic_num].ToString();
             pictureBox1.Image = pic;
+            GC.Collect();
+            check_ans = false;
+        }
+
+        private void btn_simple_test_Click(object sender, EventArgs e)
+        {
+            string str = System.IO.Directory.GetCurrentDirectory();
+
+            int number;
+            panel1.Visible = true;
+            pic_folder = str + @"\pic";
+            string[] dirs = Directory.GetDirectories(pic_folder);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                FileCollection = Directory.GetFiles(dirs[i], "*.png");
+                System.Random num = new Random();
+                for (int k = 0; k < pick_pic_num_A[i]; k++)
+                {
+                    do
+                    {
+                        number = num.Next(0, FileCollection.Length);
+                    } while (myArray.IndexOf(FileCollection[number]) != -1);
+                    myArray.Add(FileCollection[number]);
+                }
+            }
+            if (myArray.Count < 1)
+                return;
+            Bitmap pic = new Bitmap(myArray[display_pic_num].ToString());
+            now_pic_path = myArray[display_pic_num].ToString();
+            pictureBox1.Image = pic;
+            button2.Visible = true;
         }
     }
 }
